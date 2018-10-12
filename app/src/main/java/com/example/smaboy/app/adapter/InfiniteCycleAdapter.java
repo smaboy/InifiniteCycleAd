c01551c8a472c565fc90d457551c9bbd9a41414d
Smaboy <18550019225@163.com>
2018-10-12 17:36:10 +0800

    设置自定义viewpager

---
+++ app/src/main/java/com/example/smaboy/app/adapter/InfiniteCycleAdapter.java
@@ -0,0 +1,12 @@
+package com.example.smaboy.app.adapter;
+
+import android.widget.BaseAdapter;
+
+/**
+ * 类名: InfiniteCycleAdapter
+ * 类作用描述: java类作用描述
+ * 作者: Smaboy
+ * 创建时间: 2018/10/12 17:15
+ */
+public class InfiniteCycleAdapter extends BaseAdapter {
+}
---
+++ app/src/main/java/com/example/smaboy/app/view/InfiniteCycleViewPager.java
@@ -0,0 +1,12 @@
+package com.example.smaboy.app.view;
+
+import android.support.v4.view.ViewPager;
+
+/**
+ * 类名: InfiniteCycleViewPager
+ * 类作用描述: java类作用描述
+ * 作者: Smaboy
+ * 创建时间: 2018/10/12 17:14
+ */
+public class InfiniteCycleViewPager extends ViewPager {
+}
